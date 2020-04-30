16bf41e2617e2b31c184ed9e28b8d674ba2b9461
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-04-30 21:42:47 +0700

    add customer entity

---
+++ app/src/main/java/com/example/storagemanagement/model/Customer.java
@@ -0,0 +1,23 @@
+package com.example.storagemanagement.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+    private int id;
+    private String customerId;
+    private String name;
+    private String address;
+    private String phoneNumber;
+
+    public Customer(String customerId, String name, String address, String phoneNumber) {
+        this.customerId = customerId;
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+}
