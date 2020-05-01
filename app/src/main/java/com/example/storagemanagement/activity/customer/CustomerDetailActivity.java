3591a9cde0c464272e3c4e82caaf92c978e35478
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-01 10:23:34 +0700

    add customer detail layout

---
+++ app/src/main/java/com/example/storagemanagement/activity/customer/CustomerDetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.storagemanagement.activity.customer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.storagemanagement.R;
+
+public class CustomerDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_detail);
+    }
+}
