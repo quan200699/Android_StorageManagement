5ef05ecc7128f4bba6d8227fd77ad87f4028bd35
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-01 21:46:44 +0700

    add goodsDeliveryNote entity

---
+++ app/src/main/java/com/example/storagemanagement/model/goodsDeliveryNote.java
@@ -0,0 +1,27 @@
+package com.example.storagemanagement.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class goodsDeliveryNote {
+    private int id;
+    private String goodsDeliveryNoteId;
+    private String date;
+    private String customerId;
+    private String wareHouseId;
+    private String employeeId;
+    private String notice;
+
+    public goodsDeliveryNote(String goodsDeliveryNoteId, String date, String customerId, String wareHouseId, String employeeId, String notice) {
+        this.goodsDeliveryNoteId = goodsDeliveryNoteId;
+        this.date = date;
+        this.customerId = customerId;
+        this.wareHouseId = wareHouseId;
+        this.employeeId = employeeId;
+        this.notice = notice;
+    }
+}
