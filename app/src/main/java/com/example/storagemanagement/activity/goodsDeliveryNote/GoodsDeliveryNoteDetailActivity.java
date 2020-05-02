0c414c3417493816ffd6c149524499d3f7c1485e
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-02 10:08:49 +0700

    init variable

--- app/src/main/java/com/example/storagemanagement/activity/goodsDeliveryNote/GoodsDeliveryNoteDetailActivity.java
+++ app/src/main/java/com/example/storagemanagement/activity/goodsDeliveryNote/GoodsDeliveryNoteDetailActivity.java
@@ -1,16 +1,70 @@
 package com.example.storagemanagement.activity.goodsDeliveryNote;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.example.storagemanagement.R;
+import com.example.storagemanagement.dao.customer.CustomerDao;
+import com.example.storagemanagement.dao.customer.ICustomerDao;
+import com.example.storagemanagement.dao.goodsDeliveryNote.GoodsDeliveryNoteDao;
+import com.example.storagemanagement.dao.goodsDeliveryNote.IGoodsDeliveryNoteDao;
+import com.example.storagemanagement.dao.warehouse.IWarehouseDao;
+import com.example.storagemanagement.dao.warehouse.WarehouseDao;
 
 public class GoodsDeliveryNoteDetailActivity extends AppCompatActivity {
+    private EditText editTextGoodsDeliveryNoteId;
+    private EditText editTextDate;
+    private EditText editTextNotice;
+    private Spinner spinnerCustomer;
+    private Spinner spinnerWarehouse;
+    private Spinner spinnerEmployee;
+    private Button buttonEdit;
+    private Button buttonDelete;
+    private IGoodsDeliveryNoteDao goodsDeliveryNoteDao;
+    private ICustomerDao customerDao;
+    private IWarehouseDao warehouseDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goods_delivery_note_detail);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        init();
+    }
+
+    private void init() {
+        editTextGoodsDeliveryNoteId = findViewById(R.id.editTextGoodsDeliveryNoteId);
+        editTextDate = findViewById(R.id.editTextDate);
+        editTextNotice = findViewById(R.id.editTextNotice);
+        spinnerCustomer = findViewById(R.id.spinnerCustomer);
+        spinnerWarehouse = findViewById(R.id.spinnerWarehouse);
+        spinnerEmployee = findViewById(R.id.spinnerEmployee);
+        buttonDelete = findViewById(R.id.buttonDelete);
+        buttonEdit = findViewById(R.id.buttonEdit);
+        goodsDeliveryNoteDao = new GoodsDeliveryNoteDao(this);
+        customerDao = new CustomerDao(this);
+        warehouseDao = new WarehouseDao(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent intent = new Intent(GoodsDeliveryNoteDetailActivity.this, GoodsDeliveryNoteActivity.class);
+                startActivity(intent);
+                finish();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
