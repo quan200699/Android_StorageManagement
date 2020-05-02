82ed86beb452407cd053c053e9faa6459f79e3bd
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-02 17:57:23 +0700

    init variable

--- app/src/main/java/com/example/storagemanagement/activity/goodsDeliveryNoteDetail/AddGoodsDeliveryNoteDetailActivity.java
+++ app/src/main/java/com/example/storagemanagement/activity/goodsDeliveryNoteDetail/AddGoodsDeliveryNoteDetailActivity.java
@@ -1,16 +1,56 @@
 package com.example.storagemanagement.activity.goodsDeliveryNoteDetail;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.example.storagemanagement.R;
+import com.example.storagemanagement.dao.goodsDeliveryNoteDetail.GoodsDeliveryNoteDetailDao;
+import com.example.storagemanagement.dao.goodsDeliveryNoteDetail.IGoodsDeliveryNoteDetailDao;
 
 public class AddGoodsDeliveryNoteDetailActivity extends AppCompatActivity {
+    private EditText editTextGoodsDeliveryNoteId;
+    private EditText editTextQuantity;
+    private EditText editTextPrice;
+    private Spinner spinnerProduct;
+    private Button buttonSave;
+    private IGoodsDeliveryNoteDetailDao goodsDeliveryNoteDetailDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_goods_delivery_note_detail);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        init();
+    }
+
+    private void init() {
+        editTextGoodsDeliveryNoteId = findViewById(R.id.editTextGoodsDeliveryNoteId);
+        editTextQuantity = findViewById(R.id.editTextQuantity);
+        editTextPrice = findViewById(R.id.editTextPrice);
+        spinnerProduct = findViewById(R.id.spinnerProduct);
+        buttonSave = findViewById(R.id.buttonSave);
+        goodsDeliveryNoteDetailDao = new GoodsDeliveryNoteDetailDao(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent intent = new Intent(AddGoodsDeliveryNoteDetailActivity.this, GoodsDeliveryNoteDetailActivity.class);
+                startActivity(intent);
+                finish();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
