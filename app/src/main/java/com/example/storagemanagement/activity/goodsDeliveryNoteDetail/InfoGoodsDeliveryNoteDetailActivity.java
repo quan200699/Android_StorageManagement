9a8d2f5de069a2004c3f0e4224b8431eac55bddf
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-02 18:28:41 +0700

    add activity_info_goods_delivery_note_detail.xml

---
+++ app/src/main/java/com/example/storagemanagement/activity/goodsDeliveryNoteDetail/InfoGoodsDeliveryNoteDetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.storagemanagement.activity.goodsDeliveryNoteDetail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.storagemanagement.R;
+
+public class InfoGoodsDeliveryNoteDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_goods_delivery_note_detail);
+    }
+}
