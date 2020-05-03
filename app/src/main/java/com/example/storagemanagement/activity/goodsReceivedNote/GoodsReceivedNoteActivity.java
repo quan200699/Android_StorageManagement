bdd1d72c2a576bc4350923f880fe341e9311fdf5
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-03 13:44:10 +0700

    add GoodsReceivedNote layout

---
+++ app/src/main/java/com/example/storagemanagement/activity/goodsReceivedNote/GoodsReceivedNoteActivity.java
@@ -0,0 +1,16 @@
+package com.example.storagemanagement.activity.goodsReceivedNote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.storagemanagement.R;
+
+public class GoodsReceivedNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goods_received_note);
+    }
+}
