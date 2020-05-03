294b7bc0e6f0a59d14515dcddee52009654f5687
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-03 13:55:04 +0700

    add GoodsReceivedNoteActivity

--- app/src/main/java/com/example/storagemanagement/activity/MainActivity.java
+++ app/src/main/java/com/example/storagemanagement/activity/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import com.example.storagemanagement.R;
 import com.example.storagemanagement.activity.customer.CustomerActivity;
 import com.example.storagemanagement.activity.goodsDeliveryNote.GoodsDeliveryNoteActivity;
+import com.example.storagemanagement.activity.goodsReceivedNote.GoodsReceivedNoteActivity;
 import com.example.storagemanagement.activity.product.ProductActivity;
 import com.example.storagemanagement.activity.warehouse.WarehouseActivity;
 
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonCustomer;
     private Button buttonWarehouse;
     private Button buttonGoodsDeliveryNote;
+    private Button buttonGoodsReceivedNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         onClickEvent(buttonCustomer, CustomerActivity.class);
         onClickEvent(buttonWarehouse, WarehouseActivity.class);
         onClickEvent(buttonGoodsDeliveryNote, GoodsDeliveryNoteActivity.class);
+        onClickEvent(buttonGoodsReceivedNote, GoodsReceivedNoteActivity.class);
     }
 
     private void onClickEvent(Button button, final Class<?> activity) {
@@ -45,5 +48,6 @@ public class MainActivity extends AppCompatActivity {
         buttonCustomer = findViewById(R.id.buttonCustomer);
         buttonWarehouse = findViewById(R.id.buttonWarehouse);
         buttonGoodsDeliveryNote = findViewById(R.id.buttonGoodsDeliveryNote);
+        buttonGoodsReceivedNote = findViewById(R.id.buttonGoodsReceivedNote);
     }
 }
--- app/src/main/java/com/example/storagemanagement/activity/goodsReceivedNote/GoodsReceivedNoteActivity.java
+++ app/src/main/java/com/example/storagemanagement/activity/goodsReceivedNote/GoodsReceivedNoteActivity.java
@@ -1,16 +1,58 @@
 package com.example.storagemanagement.activity.goodsReceivedNote;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ListView;
 
 import com.example.storagemanagement.R;
+import com.example.storagemanagement.activity.MainActivity;
+import com.example.storagemanagement.adapter.GoodsReceivedNoteAdapter;
+import com.example.storagemanagement.dao.goodsReceivedNote.GoodsReceivedNoteDao;
+import com.example.storagemanagement.dao.goodsReceivedNote.IGoodsReceivedNoteDao;
+import com.example.storagemanagement.model.GoodsReceivedNote;
+
+import java.util.List;
 
 public class GoodsReceivedNoteActivity extends AppCompatActivity {
+    private ListView listViewGoodsReceivedNote;
+    private Button buttonAdd;
+    private IGoodsReceivedNoteDao goodsReceivedNoteDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goods_received_note);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        init();
+        final List<GoodsReceivedNote> goodsReceivedNotes = goodsReceivedNoteDao.findAll();
+        GoodsReceivedNoteAdapter goodsReceivedNoteAdapter = new GoodsReceivedNoteAdapter(GoodsReceivedNoteActivity.this, goodsReceivedNotes);
+        listViewGoodsReceivedNote.setAdapter(goodsReceivedNoteAdapter);
+
+    }
+
+    private void init() {
+        listViewGoodsReceivedNote = findViewById(R.id.listViewGoodsReceivedNote);
+        buttonAdd = findViewById(R.id.buttonAdd);
+        goodsReceivedNoteDao = new GoodsReceivedNoteDao(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent intent = new Intent(GoodsReceivedNoteActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
