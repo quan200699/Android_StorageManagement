ad1a90a3a53f367d89d239f6f348ee3c2ae57659
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-04 09:30:16 +0700

    get default value for goods received note detail done

--- app/src/main/java/com/example/storagemanagement/activity/goodsReceivedNoteDetail/InfoGoodsReceivedNoteDetailActivity.java
+++ app/src/main/java/com/example/storagemanagement/activity/goodsReceivedNoteDetail/InfoGoodsReceivedNoteDetailActivity.java
@@ -1,16 +1,122 @@
 package com.example.storagemanagement.activity.goodsReceivedNoteDetail;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.example.storagemanagement.R;
+import com.example.storagemanagement.dao.goodsReceivedNoteDetail.GoodsReceivedNoteDetailDao;
+import com.example.storagemanagement.dao.goodsReceivedNoteDetail.IGoodsReceivedNoteDetailDao;
+import com.example.storagemanagement.dao.product.IProductDao;
+import com.example.storagemanagement.dao.product.ProductDao;
+import com.example.storagemanagement.model.GoodsReceivedNoteDetail;
+import com.example.storagemanagement.model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.storagemanagement.config.StaticVariable.*;
 
 public class InfoGoodsReceivedNoteDetailActivity extends AppCompatActivity {
+    private EditText editTextGoodsReceivedNoteId;
+    private EditText editTextQuantity;
+    private EditText editTextPrice;
+    private Spinner spinnerProduct;
+    private Button buttonEdit;
+    private Button buttonDelete;
+    private IGoodsReceivedNoteDetailDao goodsReceivedNoteDetailDao;
+    private IProductDao productDao;
+    List<Product> products;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_goods_received_note_detail);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        init();
+        products = getAllProduct();
+        List<String> productNames = addProductNameToList(products);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, productNames);
+        spinnerProduct.setAdapter(adapter);
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            final GoodsReceivedNoteDetail goodsReceivedNoteDetailFromBundle = getGoodsReceivedNoteDetailFromBundle(bundle);
+            setDefaultValueForLayout(goodsReceivedNoteDetailFromBundle);
+        }
+    }
+
+    private void setDefaultValueForLayout(GoodsReceivedNoteDetail goodsReceivedNoteDetail) {
+        editTextGoodsReceivedNoteId.setText(goodsReceivedNoteDetail.getGoodsReceivedNoteId());
+        editTextPrice.setText(goodsReceivedNoteDetail.getPrice() + "");
+        editTextQuantity.setText(goodsReceivedNoteDetail.getQuantity() + "");
+        spinnerProduct.setSelection(getDefaultPositionForSpinner(products, goodsReceivedNoteDetail.getProductId()));
+    }
+
+    private int getDefaultPositionForSpinner(List<Product> products, String productId) {
+        int position = 0;
+        if (productId != null) {
+            for (int i = 0; i < products.size(); i++) {
+                if (productId.equals(products.get(i).getProductId())) {
+                    position = i + 1;
+                    break;
+                }
+            }
+        }
+        return position;
+    }
+
+    private GoodsReceivedNoteDetail getGoodsReceivedNoteDetailFromBundle(Bundle bundle) {
+        String goodsReceivedNoteId = bundle.getString(GOODS_RECEIVED_NOTE_ID);
+        int id = bundle.getInt(ID);
+        String productId = bundle.getString(PRODUCT_ID);
+        int quantity = bundle.getInt(QUANTITY);
+        double price = bundle.getDouble(PRICE);
+        return new GoodsReceivedNoteDetail(id, goodsReceivedNoteId, productId, quantity, price);
+    }
+
+    private List<String> addProductNameToList(List<Product> products) {
+        List<String> productNames = new ArrayList<>();
+        productNames.add("");
+        for (Product product : products) {
+            productNames.add(product.getName());
+        }
+        return productNames;
+    }
+
+    private List<Product> getAllProduct() {
+        return productDao.findAll();
+    }
+
+    private void init() {
+        editTextGoodsReceivedNoteId = findViewById(R.id.editTextGoodsReceivedNoteId);
+        editTextQuantity = findViewById(R.id.editTextQuantity);
+        editTextPrice = findViewById(R.id.editTextPrice);
+        spinnerProduct = findViewById(R.id.spinnerProduct);
+        buttonDelete = findViewById(R.id.buttonDelete);
+        buttonEdit = findViewById(R.id.buttonEdit);
+        goodsReceivedNoteDetailDao = new GoodsReceivedNoteDetailDao(this);
+        productDao = new ProductDao(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent intent = new Intent(InfoGoodsReceivedNoteDetailActivity.this, GoodsReceivedNoteDetailActivity.class);
+                startActivity(intent);
+                finish();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
