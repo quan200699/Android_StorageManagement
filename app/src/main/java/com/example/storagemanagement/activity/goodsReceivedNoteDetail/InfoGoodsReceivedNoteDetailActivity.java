2b0d3c7089f30f6d06ae20e63eb1853a658c2be6
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-04 09:18:46 +0700

    add activity_info_goods_received_note_detail.xml

---
+++ app/src/main/java/com/example/storagemanagement/activity/goodsReceivedNoteDetail/InfoGoodsReceivedNoteDetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.storagemanagement.activity.goodsReceivedNoteDetail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.storagemanagement.R;
+
+public class InfoGoodsReceivedNoteDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_goods_received_note_detail);
+    }
+}
