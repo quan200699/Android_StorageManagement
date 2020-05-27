1dcef74c797810881fe7ca8d7e8c5c4c9eb70981
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-27 11:17:54 +0700

    add spinner

--- app/src/main/java/com/example/storagemanagement/activity/goodsReceivedNote/AddGoodsReceivedNoteActivity.java
+++ app/src/main/java/com/example/storagemanagement/activity/goodsReceivedNote/AddGoodsReceivedNoteActivity.java
@@ -14,11 +14,17 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.storagemanagement.R;
+import com.example.storagemanagement.dao.employee.EmployeeDao;
+import com.example.storagemanagement.dao.employee.IEmployeeDao;
 import com.example.storagemanagement.dao.goodsReceivedNote.GoodsReceivedNoteDao;
 import com.example.storagemanagement.dao.goodsReceivedNote.IGoodsReceivedNoteDao;
+import com.example.storagemanagement.dao.supplier.ISupplierDao;
+import com.example.storagemanagement.dao.supplier.SupplierDao;
 import com.example.storagemanagement.dao.warehouse.IWarehouseDao;
 import com.example.storagemanagement.dao.warehouse.WarehouseDao;
+import com.example.storagemanagement.model.Employee;
 import com.example.storagemanagement.model.GoodsReceivedNote;
+import com.example.storagemanagement.model.Supplier;
 import com.example.storagemanagement.model.Warehouse;
 
 import java.util.ArrayList;
@@ -37,6 +43,8 @@ public class AddGoodsReceivedNoteActivity extends AppCompatActivity {
     private Button buttonSave;
     private IGoodsReceivedNoteDao goodsReceivedNoteDao;
     private IWarehouseDao warehouseDao;
+    private IEmployeeDao employeeDao;
+    private ISupplierDao supplierDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,8 +55,18 @@ public class AddGoodsReceivedNoteActivity extends AppCompatActivity {
         init();
         List<Warehouse> warehouses = getAllWarehouse();
         List<String> warehouseNames = addWarehouseNameToList(warehouses);
+        List<Employee> employees = getAllEmployees();
+        List<String> employeeNames = addEmployeeNameToList(employees);
+        List<Supplier> suppliers = getAllSuppliers();
+        List<String> supplierNames = addSupplierNameToList(suppliers);
         ArrayAdapter<String> adapterWarehouse = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, warehouseNames);
         spinnerWarehouse.setAdapter(adapterWarehouse);
+        ArrayAdapter<String> adapterEmployee = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, employeeNames);
+        spinnerWarehouse.setAdapter(adapterWarehouse);
+        ArrayAdapter<String> adapterSupplier = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, supplierNames);
+        spinnerWarehouse.setAdapter(adapterWarehouse);
+        spinnerSupplier.setAdapter(adapterSupplier);
+        spinnerEmployee.setAdapter(adapterEmployee);
         buttonSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -97,8 +115,34 @@ public class AddGoodsReceivedNoteActivity extends AppCompatActivity {
         return warehouseNames;
     }
 
+    private List<String> addEmployeeNameToList(List<Employee> employees) {
+        List<String> employeeNames = new ArrayList<>();
+        employeeNames.add("");
+        for (Employee employee : employees) {
+            employeeNames.add(employee.getName());
+        }
+        return employeeNames;
+    }
+
+    private List<String> addSupplierNameToList(List<Supplier> suppliers) {
+        List<String> supplierNames = new ArrayList<>();
+        supplierNames.add("");
+        for (Supplier supplier : suppliers) {
+            supplierNames.add(supplier.getName());
+        }
+        return supplierNames;
+    }
+
     private List<Warehouse> getAllWarehouse() {
         return warehouseDao.findAll();
+    }
+
+    private List<Supplier> getAllSuppliers() {
+        return supplierDao.findAll();
+    }
+
+    private List<Employee> getAllEmployees() {
+        return employeeDao.findAll();
     }
 
     private void init() {
@@ -111,6 +155,8 @@ public class AddGoodsReceivedNoteActivity extends AppCompatActivity {
         buttonSave = findViewById(R.id.buttonSave);
         goodsReceivedNoteDao = new GoodsReceivedNoteDao(this);
         warehouseDao = new WarehouseDao(this);
+        employeeDao = new EmployeeDao(this);
+        supplierDao = new SupplierDao(this);
     }
 
     @Override
