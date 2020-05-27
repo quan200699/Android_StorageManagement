1d3c0acfd033e67eb6e741ed5e2ab5b3c2ab4081
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-27 10:29:46 +0700

    employee activity

--- app/src/main/java/com/example/storagemanagement/activity/employee/EmployeeActivity.java
+++ app/src/main/java/com/example/storagemanagement/activity/employee/EmployeeActivity.java
@@ -6,11 +6,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ListView;
 
 import com.example.storagemanagement.R;
 import com.example.storagemanagement.activity.MainActivity;
+import com.example.storagemanagement.adapter.EmployeeAdapter;
+import com.example.storagemanagement.dao.employee.EmployeeDao;
+import com.example.storagemanagement.dao.employee.IEmployeeDao;
+import com.example.storagemanagement.model.Employee;
+
+import java.util.List;
 
 public class EmployeeActivity extends AppCompatActivity {
+    private ListView listViewEmployee;
+    private Button buttonAdd;
+    private IEmployeeDao employeeDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +29,16 @@ public class EmployeeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_employee);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+        init();
+        final List<Employee> customers = employeeDao.findAll();
+        EmployeeAdapter customerAdapter = new EmployeeAdapter(EmployeeActivity.this, customers);
+        listViewEmployee.setAdapter(customerAdapter);
+    }
+
+    private void init() {
+        listViewEmployee = findViewById(R.id.listViewEmployee);
+        buttonAdd = findViewById(R.id.buttonAdd);
+        employeeDao = new EmployeeDao(this);
     }
 
     @Override
---
+++ app/src/main/java/com/example/storagemanagement/adapter/EmployeeAdapter.java
@@ -0,0 +1,60 @@
+package com.example.storagemanagement.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.storagemanagement.R;
+import com.example.storagemanagement.model.Employee;
+
+import java.util.List;
+
+public class EmployeeAdapter extends BaseAdapter {
+    private Context context;
+    private List<Employee> employees;
+
+    public EmployeeAdapter(Context context, List<Employee> employees) {
+        this.context = context;
+        this.employees = employees;
+    }
+
+    @Override
+    public int getCount() {
+        return employees.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return employees.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater layoutInflater = LayoutInflater.from(context);
+            convertView = layoutInflater.inflate(R.layout.employee_row_layout, null);
+        }
+        Employee employee = (Employee) getItem(position);
+        if (employee != null) {
+            TextView textViewEmployeeId = convertView.findViewById(R.id.textViewEmployeeId);
+            TextView textViewEmployeeName = convertView.findViewById(R.id.textViewEmployeeName);
+            TextView textViewBirthday = convertView.findViewById(R.id.textViewBirthday);
+            TextView textViewSex = convertView.findViewById(R.id.textViewSex);
+            TextView textViewAddress = convertView.findViewById(R.id.textViewSex);
+            textViewEmployeeId.setText(employee.getEmployeeId());
+            textViewEmployeeName.setText(employee.getName());
+            textViewBirthday.setText(employee.getAddress());
+            textViewSex.setText(employee.getSex());
+            textViewAddress.setText(employee.getAddress());
+        }
+        return convertView;
+    }
+}
